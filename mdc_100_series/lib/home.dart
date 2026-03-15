e5552353d42bb84678faa376e34f3ec64809128b
edwinjosue00123 <0502200804481@iteesa.edu.hn>
2026-03-15 17:09:05 -0600

    Finalizado paso 4  del codelab

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -1,73 +1,19 @@
-// Copyright 2018-present the Flutter authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'supplemental/asymmetric_view.dart';
 import 'model/product.dart';
 import 'model/products_repository.dart';
 
 class HomePage extends StatelessWidget {
-const HomePage({Key? key}) : super(key: key);
+  final Category category;
 
-// TODO: Make a collection of cards (102)
-// TODO: Make a collection of cards (102)
+  const HomePage({Key? key, required this.category}) : super(key: key);
 
-// Replace this entire method
-
-// TODO: Add a variable for Category (104)
 @override
 Widget build(BuildContext context) {
-// TODO: Return an AsymmetricView (104)
-// TODO: Pass Category variable to AsymmetricView (104)
 return Scaffold(
-// TODO: Add app bar (102)
-appBar: AppBar(
-actions: <Widget>[
-IconButton(
-onPressed: () {
-print('Search Button');
-},
-icon: const Icon(
-Icons.search,
-semanticLabel: 'search',
+body: AsymmetricView(
+        products: ProductsRepository.loadProducts(category),
 ),
-),
-IconButton(
-onPressed: () {
-print('Filter Button');
-},
-icon: const Icon(Icons.tune, semanticLabel: 'filter'),
-),
-],
-leading: IconButton(
-icon: const Icon(
-Icons.menu,
-semanticLabel: 'menu',
-),
-onPressed: () {
-print('Menu Button');
-},
-),
-title: const Text('SHRINE'),
-),
-// TODO:
-//Add a grid view (102)
-      body: AsymmetricView(
-        products: ProductsRepository.loadProducts(Category.all),
-),
-
-// TODO: Set resizeToAvoidBottomInset (101)
 resizeToAvoidBottomInset: false,
 );
 }
