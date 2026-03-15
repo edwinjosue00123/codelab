0c33ba280b0fe511eb9fbd5b1f023c84c4b70600
edwinjosue00123 <0502200804481@iteesa.edu.hn>
2026-03-15 16:36:08 -0600

    Finalizado paso del codelab 101#

--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -15,36 +15,75 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+const LoginPage({Key? key}) : super(key: key);
 
-  @override
-  _LoginPageState createState() => _LoginPageState();
+@override
+_LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  // TODO: Add text editing controllers (101)
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          children: <Widget>[
-            const SizedBox(height: 80.0),
-            Column(
-              children: <Widget>[
-                Image.asset('assets/diamond.png'),
-                const SizedBox(height: 16.0),
-                const Text('SHRINE'),
-              ],
-            ),
-            const SizedBox(height: 120.0),
-            // TODO: Remove filled: true values (103)
+// TODO: Add text editing controllers (101)
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+body: SafeArea(
+child: ListView(
+padding: const EdgeInsets.symmetric(horizontal: 24.0),
+children: <Widget>[
+const SizedBox(height: 80.0),
+Column(
+children: <Widget>[
+Image.asset('assets/diamond.png'),
+const SizedBox(height: 16.0),
+const Text('SHRINE'),
+],
+),
+const SizedBox(height: 120.0),
+// TODO: Remove filled: true values (103)
             // TODO: Add TextField widgets (101)
-            // TODO: Add button bar (101)
-          ],
-        ),
-      ),
-    );
-  }
+
+            // TODO: Add TextField widgets (101)            //Name
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'username',
+              ),
+            ),
+            //spacer
+            const SizedBox(height: 12.0),
+            // Password
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+// TODO: Add button bar (101)
+            OverflowBar(
+              alignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Text('CANCEL'),
+                  onPressed: () {
+                    _usernameController.clear();
+                    _passwordController.clear();
+                  },
+                ),
+                ElevatedButton(
+                    child: const Text("NEXT"),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    })
+              ],
+            )
+],
+),
+),
+);
+}
 }
