af8ad0f7df256c1084260dbd70eee21b10de0413
edwinjosue00123 <0502200804481@iteesa.edu.hn>
2026-03-15 16:52:13 -0600

    Finalizado paso pantalla inicio creo del codelab

--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -1,89 +1,71 @@
-// Copyright 2018-present the Flutter authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
-const LoginPage({Key? key}) : super(key: key);
+  const LoginPage({Key? key}) : super(key: key);
 
-@override
-_LoginPageState createState() => _LoginPageState();
+  @override
+  _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-// TODO: Add text editing controllers (101)
-  final _usernameController = TextEditingController();
-  final _passwordController = TextEditingController();
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-body: SafeArea(
-child: ListView(
-padding: const EdgeInsets.symmetric(horizontal: 24.0),
-children: <Widget>[
-const SizedBox(height: 80.0),
-Column(
-children: <Widget>[
-Image.asset('assets/diamond.png'),
-const SizedBox(height: 16.0),
-const Text('SHRINE'),
-],
-),
-const SizedBox(height: 120.0),
-// TODO: Remove filled: true values (103)
-            // TODO: Add TextField widgets (101)
-
-            // TODO: Add TextField widgets (101)            //Name
-            TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                filled: true,
-                labelText: 'username',
-              ),
-            ),
-            //spacer
-            const SizedBox(height: 12.0),
-            // Password
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                filled: true,
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
-// TODO: Add button bar (101)
-            OverflowBar(
-              alignment: MainAxisAlignment.end,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            SizedBox(height: 80.0),
+            Column(
               children: <Widget>[
-                TextButton(
-                  child: const Text('CANCEL'),
-                  onPressed: () {
-                    _usernameController.clear();
-                    _passwordController.clear();
-                  },
-                ),
-                ElevatedButton(
-                    child: const Text("NEXT"),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    })
+                FlutterLogo(size: 100), // Aquí irá el logo luego
+                SizedBox(height: 16.0),
+                Text('SHRINE'),
               ],
-            )
-],
+            ),
+            SizedBox(height: 120.0),
+            // Campo de Usuario
+TextField(
+  decoration: const InputDecoration(
+    filled: true,
+    labelText: 'Username',
+  ),
 ),
+// Espacio entre campos
+const SizedBox(height: 12.0),
+// Campo de Contraseña
+TextField(
+  decoration: const InputDecoration(
+    filled: true,
+    labelText: 'Password',
+  ),
+  obscureText: true, // Esto oculta los caracteres (puntos)
 ),
-);
-}
+
+            // TODO: Añadir TextField para Password (Paso 3 del codelab)
+            const SizedBox(height: 12.0),
+OverflowBar(
+  alignment: MainAxisAlignment.end,
+  children: <Widget>[
+    TextButton(
+      child: const Text('CANCEL'),
+      onPressed: () {
+        // Al presionar, se limpian los campos
+      },
+    ),
+    ElevatedButton(
+      child: const Text('NEXT'),
+      onPressed: () {
+        // Al presionar, navegamos a la siguiente pantalla (MDC-102)
+        Navigator.pop(context);
+      },
+    ),
+  ],
+),
+          ],
+        ),
+      ),
+    );
+  }
 }
